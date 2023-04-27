cd1527c2275a7f5b319dceeeb0541ae0a03616c8
shashank <shashank944533@gmail.com>
2023-04-27 10:06:58 +0530

    word count, char count, preview and summary of text updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">{props.title}</a>
-    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
-    </button> */}
+    </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
@@ -21,12 +21,10 @@ export default function Navbar(props) {
       </ul>
       <form className="d-flex">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <button className="btn btn-danger" type="submit">Search</button>
       </form>
     </div>
   </div>
-
-
 </nav>
   )
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,23 +10,42 @@ export default function TextForm(props) {
         setText(newText)
     }
 
+    const handleLoClick = () => {
+        console.log("LowerCase was clicked!" + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
     const handleOnChange =(event)=> {
         console.log("On Change");
         setText(event.target.value);
     }
 
-    const [text, setText] = useState("Enter text here 100");
+    const [text, setText] = useState("");
 
     
   return (
-
-    <div>
+    <>
+        <div>
+        
             <h1>{props.heading}</h1>
             <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="4"></textarea>
             </div>
 
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
-    </div>
+            <button className="btn btn-primary  mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary  mx-2 " onClick={handleLoClick}>Convert to LowerCase</button>
+
+        </div>
+
+        <div className="container my-5">
+        <h3>Your text summary</h3>
+        <p>{text.split(" ").length} <b>words</b>, {text.length} <b>characters</b></p>
+        <p>{ 0.008 * text.split(" ").length} Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+
+        </div>
+    </>
   )
 }
